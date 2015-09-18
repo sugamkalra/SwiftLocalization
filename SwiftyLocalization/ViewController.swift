b2e35f9694f1a6a295cb7dcc5de125f9239bfcd6
Sugam Kalra <sugamkalra@Sugams-MacBook-Pro.local>
2015-09-18 19:44:14 +0530

    Initial Commit

---
+++ SwiftyLocalization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyLocalization
+//
+//  Created by Sugam Kalra on 18/09/15.
+//  Copyright (c) 2015 Sugam Kalra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
